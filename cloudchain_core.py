9fa8f147fda10fba8ad76cc454e8d117b70328a3
ParkerDinkins <parkerdinkins@gmail.com>
2019-01-31 18:13:43 -0500

    0.1.2

---
+++ cloudchain_core.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""Main module."""
+##  MUST INSTALL ALL OF THE FOLLOWING MODULES ##
+    # pdinkins/node-server 
+    # pdinkins/terminal-client
+    # pdinkins/pythos
+    # pdinkins/branching-blockchain
+
+    # go-lang
+    # go-ipfs
+
+    # ipfsapi
+    # requests
+
+# IMPORTS #
+
+# python library imports 
+import os
+import sys
+import platform
+import shutil
+import datetime
+
+# third party python imports
+import ipfsapi
+import requests
+
+# main script sequence that spawns subsequent processes decribed below
+    
+    # Establish setup sequence
+    # User login sequence 
--- docs/HISTORY.rst
+++ docs/HISTORY.rst
@@ -1,5 +1,10 @@
 History
 
-0.1.0 (1/20/19)
+0.1.0-1.20.19
 * Initial Github commit.
 
+0.1.1-1.30.19
+* core lib creation
+
+0.1.2-1.31.19
+* documentation consolidation 
