75eeea0e0f55ddd6e5256081e8b0eb15d32b4053
ParkerDinkins <parkerdinkins@gmail.com>
2019-02-07 23:47:58 -0500

    logger module

---
+++ core/logger/main.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""core.logger.main module."""
+
+# IMPORTS #
+
+# python library imports
+import logging
+import inspect
+import datetime
+
+class CORE_LOGGER:
+    def __init__(self):
+        self.log = self.__log
+        
+    
+    def __log(self, message):
+        self.__current_function = inspect.currentframe().f_back.f_code
+        logging.basicConfig(level=logging.DEBUG, format='%(name)s: %(messsage)s',)
+        return logging.debug("{}\t{}\t{}\t{}".format(
+            datetime.datetime.now(),
+            self.__current_function.co_filename,
+            self.__current_function.co_name,
+            message))
