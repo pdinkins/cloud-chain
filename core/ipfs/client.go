78197ddc39bb1bd736889dbaa9a647605c56ebf7
pdinkins <parkerdinkins@gmail.com>
2019-02-13 21:18:34 -0500

    0.1.27

---
+++ core/ipfs/client.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	core "github.com/ipfs/go-ipfs/core"
+	corenet "github.com/ipfs/go-ipfs/core/corenet"
+	peer "github.com/ipfs/go-ipfs/p2p/peer"
+	fsrepo "github.com/ipfs/go-ipfs/repo/fsrepo"
+
+	"golang.org/x/net/context"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Please give a peer ID as an argument")
+		return
+	}
+	target, err := peer.IDB58Decode(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	// Basic ipfsnode setup
+	r, err := fsrepo.Open("~/.ipfs")
+	if err != nil {
+		panic(err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	cfg := &core.BuildCfg{
+		Repo:   r,
+		Online: true,
+	}
+
+	nd, err := core.NewNode(ctx, cfg)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("I am peer %s dialing %s\n", nd.Identity, target)
+
+	con, err := corenet.Dial(nd, target, "/app/whyrusleeping")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	io.Copy(os.Stdout, con)
+}
