2220ef318aa341c1a09a682172dafb94ab3215d7
pdinkins <parkerdinkins@gmail.com>
2019-06-01 10:53:55 -0400

    0.1.396

--- core/network/utils/ingest.py
+++ core/network/utils/ingest.py
@@ -11,14 +11,19 @@ class INGEST:
     def __init__(self, host_ip):
         self.ip = host_ip
         self.ingestion = self._ingestion()
+        self._socket_established = False
     
     def _ingestion(self):
-        soc2sniff = socket(AF_INET, SOCK_RAW, IPPROTO_IP)
-        soc2sniff.bind((self.ip, 0))
-        soc2sniff.setsockopt(IPPROTO_IP, IP_HDRINCL, 1)
-        soc2sniff.ioctl(SIO_RCVALL, RCVALL_ON)
-        return soc2sniff
+        try:
+            soc2sniff = socket(AF_INET, SOCK_RAW, IPPROTO_IP)
+            soc2sniff.bind((self.ip, 0))
+            soc2sniff.setsockopt(IPPROTO_IP, IP_HDRINCL, 1)
+            soc2sniff.ioctl(SIO_RCVALL, RCVALL_ON)
+            self._socket_established = True
+            return soc2sniff
+        except OSError:
+            print("Must use admin shell")
     
     def _return_raw_packets(self):
-        while True:
+        while self._socket_established:
             return self.ingestion.recvfrom(65565)
