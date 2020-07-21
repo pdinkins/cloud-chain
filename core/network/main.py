fb0d4aba7c4bd5e9f03bed5135533962053f2635
pdinkins <parkerdinkins@gmail.com>
2020-07-20 23:09:32 -0400

    0.1.398

--- core/network/main.py
+++ core/network/main.py
@@ -11,6 +11,70 @@ class CORE_NETWORK:
     def __init__(self):
         self.ip = HOST._get_host_ip()
 
+
+class network:
+    """
+    Networks consist of a collection of nodes. Each node has a list of peers that
+    it communicates with. 
+
+    """
+    def __init__(self):
+        self.load_nodes()
+    
+    def load_nodes(self):
+        pass
+
+
+class node:
+    '''
+    A node is a single entity within the network. A node can be a single computer 
+    or multiple within the same LAN.
+
+    ip: must be an ip object
+    peers: list of ip's
+
+    '''
+    def __init__(self, ip, peers):
+        self.ip = str(ip) 
+        self.peers = list(peers)
+
+class peer:
+    '''
+    A peer is a node that is trusted by another node in the network. Data is transmitted directly
+    to peers from node through sockets. 
+
+    ip: must be an ip object
+    '''
+    def __init__(self, ip):
+        self.ip = ip
+        if self.ip.object:
+            print("object test pass")
+        else: 
+            print("object test fail. you must use an ip object")
+
+class ip:
+    '''
+    This class is used to create a single data structure to use when working with ip addresses.
+
+    The octect are passed when the class is created and then the .address attribute allows for 
+    easy use of the ip object for others functions.
+    '''
+    def __init__(self, octet1, octet2, octet3, octet4):
+        self.octet1 = str(octet1)
+        self.octet2 = str(octet2)
+        self.octet3 = str(octet3)
+        self.octet4 = str(octet4)
+        self.ipaddress = self.__ipaddress()
+        self.object = True
+
+    def __ipaddress(self):
+        self.__ip = str(
+            self.octet1 + '.' +
+            self.octet2 + '.' +
+            self.octet3 + '.' +
+            self.octet4)
+        return self.__ip
+
 def test_network():
     c = CORE_NETWORK()
     while True:
